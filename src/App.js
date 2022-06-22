dc92d6bfda9e7096d95cc25a69c28326700a8122
Thulasi <tganesh07@gmail.com>
2022-06-22 18:17:57 -0400

    Adding route to page not found

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+import { Route, Routes, BrowserRouter as Router, Navigate } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import AboutMe from "./page/AboutMe";
 import Blogs from "./page/Blogs";
 import Home from "./page/Home";
+import NotFound from "./page/NotFound";
 
 function App() {
     return (
@@ -12,6 +13,8 @@ function App() {
                 <Route path="/" exact element={<Home />} />
                 <Route path="/about" element={<AboutMe />} />
                 <Route path="/blog" element={<Blogs />} />
+                <Route path="/not-found" element={<NotFound />} />
+                <Route path="*" element={<Navigate to="not-found" />} />
             </Routes>
         </Router>
     );
