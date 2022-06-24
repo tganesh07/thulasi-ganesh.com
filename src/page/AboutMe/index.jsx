ec1aa1dc5f8e60ff6d7589252a1d588fdbac7f89
Thulasi <tganesh07@gmail.com>
2022-06-23 20:51:52 -0400

    Added first section to about me file

--- src/page/AboutMe/index.jsx
+++ src/page/AboutMe/index.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const AboutMe = () => {
-    return <h1> I learn, code and teach!</h1>;
+    return (
+        <section>
+            <h1> Hi! I'm Thulasi Ganesh</h1>
+            <h3> I learn, code and teach!</h3>
+        </section>
+    );
 };
 
 export default AboutMe;
