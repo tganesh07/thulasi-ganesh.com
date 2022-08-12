34f9370ca336ce07254349ce2d23009f39e78b74
Thulasi <tganesh07@gmail.com>
2022-08-11 20:20:16 -0400

    read blog metadata from object array

--- src/page/Blogs/index.js
+++ src/page/Blogs/index.js
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from "react";
 import Markdown from "markdown-to-jsx";
 
-const Blogs = () => {
+const Blog = ({ fileName }) => {
     const [post, setPost] = useState("");
-    const fileName = "Top5VSCodeExtensions.md";
 
     useEffect(() => {
         import(`./markdown/${fileName}`).then((res) => {
@@ -12,8 +11,30 @@ const Blogs = () => {
                 .then((res) => setPost(res))
                 .catch((err) => console.log(err));
         });
-    }, []);
+    });
     return <Markdown>{post}</Markdown>;
+};
+
+const Blogs = () => {
+    const [posts, setPosts] = useState([
+        {
+            fileName: "Top5VSCodeExtensions.md",
+            title: "Top 5 VS Code extensions for Web Development",
+            desc: "It is the year 2022 and Microsoft's Visual Studio Code is the best IDE for Web Developers.",
+        },
+        { fileName: 2, title: "React Js shortcuts", desc: "React js is simply beautiful" },
+    ]);
+
+    return (
+        <div className="blog">
+            {posts.map((post) => (
+                <div key={post.fileName} className="blog__item" onClick={() => console.log(`clicked ${post.fileName}`)}>
+                    <h3>{post.title}</h3>
+                    <p>{post.desc}</p>
+                </div>
+            ))}
+        </div>
+    );
 };
 
 export default Blogs;
